46134042c7cafc0cdd5ef4786e4147dd73fba4b1
Илья Аленин <aleninIlya@outlook.com>
2020-12-20 22:24:20 +0300

    All done

--- Second/Program.cs
+++ Second/Program.cs
@@ -10,6 +10,23 @@ namespace Second
     {
         static void Main(string[] args)
         {
+            // Инициализация списка строк c погодой
+            List<string> Wether = new List<string>() { "солнечная", "пасмурная", "дождливая" };
+            DateTime date = DateTime.Now;
+            Console.Write("Сейчас {0:%h} часов", date);
+            int hour = date.Hour;
+            if(hour < 6 || hour > 22)
+                Console.Write("ночи.");
+            else if(hour < 10)
+                Console.Write("утра.");
+            else if(hour < 15)
+                Console.Write("дня.");
+            else
+                Console.Write("вечера");
+            int index = (new Random()).Next(0, 3);
+            Console.Write($" Погода {Wether[index]}.");
+            Console.ReadKey();
+
         }
     }
 }
