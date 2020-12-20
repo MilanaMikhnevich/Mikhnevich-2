59625d29c7b0dc0a2352f025adb76434a8bfe5b4
Илья Аленин <aleninIlya@outlook.com>
2020-12-20 22:26:08 +0300

    The correct output

--- Second/Program.cs
+++ Second/Program.cs
@@ -13,7 +13,7 @@ namespace Second
             // Инициализация списка строк c погодой
             List<string> Wether = new List<string>() { "солнечная", "пасмурная", "дождливая" };
             DateTime date = DateTime.Now;
-            Console.Write("Сейчас {0:%h} часов", date);
+            Console.Write("Сейчас {0:%h} часов ", date);
             int hour = date.Hour;
             if(hour < 6 || hour > 22)
                 Console.Write("ночи.");
@@ -22,7 +22,7 @@ namespace Second
             else if(hour < 15)
                 Console.Write("дня.");
             else
-                Console.Write("вечера");
+                Console.Write("вечера.");
             int index = (new Random()).Next(0, 3);
             Console.Write($" Погода {Wether[index]}.");
             Console.ReadKey();
